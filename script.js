8d604d053bdd968bbee549e02218b9cb6936a097
milon2000 <milakozlowska2000@gmail.com>
2021-07-14 11:47:09 +0200

    loading the visitor's location

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-
-
 // & value of input
 const submit = document.querySelector('.submit');
 const ip = document.querySelector('#ip');
+
+// & funkcja, ktora pokazuje lokalizacje
+function createMap(data) {
+    let view = [data.location.lat, data.location.lng];
+    map.setView(view, 13);
+    L.marker(view).addTo(map);
+}
+
+// & pokazuje lokalizacje po zaladowaniu strony
+
+function getIP(json) {
+    let url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&ipAddress=' + json.ip;
+    getLocation(url);
+    
+}
+
+// & sciaga url z danymi
 
 async function getLocation(url) {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data.location.country);
-    
+    createMap(data);
 }
 
-
-
-
-function sendIP(e) {
+// & po submit tworzy url do funkcji getLocation
+function checkIP(e) {
+    const ipValue = ip.value;
     let url;
-e.preventDefault();
-const ipValue = ip.value;
-//const api_key ="at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8";
+    e.preventDefault();
+    if (/^\d+$/.test(ipValue) === true) {
+        url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&ipAddress=' + ipValue;
+    } else if (ipValue.includes('@')) {
+        url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&email=' + ipValue;
+    } else {
+        url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&domain=' + ipValue;
+    }
 
-if(/^\d+$/.test(ipValue) === true) {
-     url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&ipAddress=' + ipValue;
-} else if( ipValue.includes('@')) {
-    url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&email=' + ipValue;
-} else {url = 'https://geo.ipify.org/api/v1?apiKey=at_4BM9B1l7yGQYtDcxk5yzFRXsxVPY8&domain=' + ipValue;}
-
-
-getLocation(url);
-
+    getLocation(url);
 }
 
 
-submit.addEventListener('click', sendIP);
+submit.addEventListener('click', checkIP);
 
 
 
-// & Lokalizacja
+// & tworzenie mapy
 
+let map = L.map('map');
 
-
-
-
-// & Map
-const map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlsb24yMDAwIiwiYSI6ImNrcjF2bDNwdzI1dHoyb28xczQzNmN6MjMifQ.EfwrN3LYy6p-fAy7Wt68UA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -55,6 +62,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 
 
+ 
 
 
 
